c55f798d2ffe09f3b2c9e1212dee63dec105b941
Barry Tji <barry.tji@gmail.com>
2025-03-18 17:05:29 +0800

    v1.0.4

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,9 +6,10 @@ plugins {
 }
 
 android {
-    namespace = "com.example.asd_care_app"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    namespace = "com.infoex.asdCare"
+
+    compileSdk = 34 // ✅ 直接 Hardcode
+    ndkVersion = "27.0.12077973" // ✅ 直接 Hardcode NDK 版本
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -24,8 +25,8 @@ android {
         applicationId = "com.example.asd_care_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = 21
+        targetSdk = 34
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
